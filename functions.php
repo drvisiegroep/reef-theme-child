e03ee1fa1e4cc3899265b99d07f06474b5a3bd48
drvisiegroep <78969608+drvisiegroep@users.noreply.github.com>
2022-01-25 20:56:34 +0100

    scripts naar enqueue custom

--- functions.php
+++ functions.php
@@ -19,24 +19,4 @@ function site_settings(){
 include( get_stylesheet_directory() . '/inc/custom/custom-editor-options.php');
 include( get_stylesheet_directory() . '/inc/custom/custom-theme-support.php');
 include( get_stylesheet_directory() . '/inc/custom/custom-widgets.php');
-
-
-//
-// Laad scripts voor child thema
-//
-function child_scripts() {
-    
-    wp_enqueue_style( 'css-reef-theme-child', get_stylesheet_directory_uri() . '/assets/css/child.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/child.css' ) );
-    wp_enqueue_script( 'js-reef-theme-child', get_stylesheet_directory_uri() . '/assets/js/child.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/assets/js/child.js' ), true );
-
-}
-add_action( 'wp_enqueue_scripts', 'child_scripts', 99);
-
-
-
-
-
-
-
-
-
+include( get_stylesheet_directory() . '/inc/custom/custom-enqueue.php');
--- inc/custom/custom-enqueue.php
+++ inc/custom/custom-enqueue.php
@@ -1,7 +1,18 @@
 <?php
-function reef_editor_styles () {
 
-    add_editor_style( 'assets/css/child.css' );
+//
+// Laad scripts voor child thema
+//
+function child_scripts() {
+    
+    wp_enqueue_style( 'css-reef-theme-child', get_stylesheet_directory_uri() . '/assets/css/child.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/child.css' ) );
+    wp_enqueue_script( 'js-reef-theme-child', get_stylesheet_directory_uri() . '/assets/js/child.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/assets/js/child.js' ), true );
+
+}
+add_action( 'wp_enqueue_scripts', 'child_scripts', 99);
+
+function reef_child_editor_styles() {
+    add_editor_style('assets/css/child.css');
     add_editor_style( 'https://fonts.googleapis.com/css?family=Roboto:wght@300;400;700&display=swap' );
 }
-add_action( 'enqueue_block_editor_assets', 'reef_editor_styles', 99);
+add_action( 'after_setup_theme', 'reef_child_editor_styles');

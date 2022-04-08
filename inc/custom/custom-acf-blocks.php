a1bf7c587815140a9182ce189e82babbcddb5e2d
daniel <daniel@websiteaanbieder.nl>
2022-04-08 13:42:58 +0200

    voorbeeld custom acf blocks toegevoegd

--- inc/custom/custom-acf-blocks.php
+++ inc/custom/custom-acf-blocks.php
@@ -1,84 +1,41 @@
 <?php
 //
-// Image Slider
+// Child theme
+// Custom | ACF Blocks 
 //
-add_action('acf/init', 'image_slider_block_init');
-function image_slider_block_init() {
+// @package      reef-theme
+// @author       Daniël R.
+// @since        1.0.0
+//
+// Voorbeeld functie voor een custom ACF blok
+//
+// add_action('acf/init', 'je_functienaam');
+// function je_functienaam() {
     
-    // Check function exists.
-    if( function_exists('acf_register_block_type') ) {
+//     // Check function exists.
+//     if( function_exists('acf_register_block_type') ) {
         
-        // Register a image-slider block.
-        acf_register_block_type(array(
-            'name'              => 'image-slider',
-            'title'             => __('image-slider'),
-            'description'       => __('A custom before-after image-slider block.'),
-            'render_template'   => 'inc/custom/custom-acf-blocks/image-slider/image-slider.php',
-            'category'          => 'formatting',
-            'mode'              => 'auto',
-            'enqueue_style'     => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/image-slider/css/image_slider.css',
-            'enqueue_script'    => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/image-slider/js/image_slider.js',
-        ));
-    }
-}
+//         // Register a CUSTOMNAME block.
+//         acf_register_block_type(array(
+//             'name'              => 'CUSTOMNAME',
+//             'title'             => __('CUSTOMNAME'),
+//             'description'       => __('A custom before-after CUSTOMNAME block.'),
+//             'render_template'   => 'inc/custom/custom-acf-blocks/CUSTOMNAME/CUSTOMNAME.php',
+//             'category'          => 'formatting',
+//             'mode'              => 'auto',
+//             'enqueue_style'     => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/CUSTOMNAME/css/CUSTOMNAME.css',
+//             'enqueue_script'    => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/CUSTOMNAME/js/CUSTOMNAME.js',
+//             'supports'	    	=> [
+//                 'align'		    	=> false,
+//                 'anchor'	    	    => true,
+//                 'customClassName'	=> true,
+//                 'jsx' 		    	=> true,
+//             ]
+//         ));
+//     }
+// }
 
-//
-// Child page shortcode ACF-i-fied
-//
-add_action('acf/init', 'child_pages_block_init');
-function child_pages_block_init() {
-    
-    // Check function exists.
-    if( function_exists('acf_register_block_type') ) {
-        
-        // Register a child-pages block.
-        acf_register_block_type(array(
-            'name'              => 'child-pages',
-            'title'             => __('child-pages'),
-            'description'       => __('A custom before-after child-pages block.'),
-            'render_template'   => 'inc/custom/custom-acf-blocks/child-pages/child-pages.php',
-            'category'          => 'formatting',
-            'mode'              => 'auto',
-            'enqueue_style'     => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/child-pages/css/child_pages.css',
-            'enqueue_script'    => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/child-pages/js/child_pages.js',
-            'supports'	    	=> [
-                'align'		    	=> false,
-                'anchor'	    	=> true,
-                'customClassName'	=> true,
-                'jsx' 		    	=> true,
-            ]
-        ));
-    }
-}
 
-//
-// Premade wide text and media block
-//
-add_action('acf/init', 'custom_wide_tm_block_block_init');
-function custom_wide_tm_block_block_init() {
-    
-    // Check function exists.
-    if( function_exists('acf_register_block_type') ) {
-        
-        // Register a child-pages block.
-        acf_register_block_type(array(
-            'name'              => 'custom-wide-tm-block',
-            'title'             => __('custom-wide-tm-block'),
-            'description'       => __('A custom-wide-tm-block.'),
-            'render_template'   => 'inc/custom/custom-acf-blocks/custom-wide-tm-block/custom-wide-tm-block.php',
-            'category'          => 'formatting',
-            'mode'              => 'auto',
-            'enqueue_style'     => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/custom-wide-tm-block/css/custom-wide-tm-block.css',
-            'enqueue_script'    => get_stylesheet_directory_uri() . '/inc/custom/custom-acf-blocks/custom-wide-tm-block/js/custom-wide-tm-block.js',
-            'supports'	    	=> [
-                'align'		    	=> true,
-                'anchor'	    	=> true,
-                'customClassName'	=> true,
-                'jsx' 		    	=> true,
-            ]
-        ));
-    }
-}
 
 
 

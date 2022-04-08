8517364ded84d56f3f07da49673e17abbe716d7f
daniel <daniel@websiteaanbieder.nl>
2022-04-08 13:51:39 +0200

    Create template-custom-template.php
    custom pagina voorbeeld  template toegevoegd

---
+++ page-templates/template-custom-template.php
@@ -0,0 +1,13 @@
+<?php
+//
+// Template Name: custom-template
+// Template Post Type: page
+//
+// https://developer.wordpress.org/themes/template-files-section/page-template-files/#file-organization-of-page-templates
+//
+
+
+require get_template_directory() . '/index.php';
+
+
+?>

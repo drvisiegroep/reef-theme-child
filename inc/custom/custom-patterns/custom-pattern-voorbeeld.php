3d9985bf27a2d726c7e8ff4a03532730aa830ead
daniel <daniel@websiteaanbieder.nl>
2022-04-08 13:44:45 +0200

    Create custom-pattern-voorbeeld.php
    custom pagina template toegevoegd.

---
+++ inc/custom/custom-patterns/custom-pattern-voorbeeld.php
@@ -0,0 +1,67 @@
+<?php
+// register_block_pattern(
+//     'custom/four-panel-media-text-dark-first',
+//     array(
+//         'title'         => __( 'Custom - 4 panel M/T - Dark first', 'reef-theme' ),
+//         'description'   => _x( '4 Panel Media and Text', '4 Panels of Media and Text', 'reef-theme' ),
+//         'categories'    => array( 'custom' ),
+//         'keywords'      => array( 'custom', 'media', 'text', 'dark' ),
+//         'content'       => '<!-- wp:group {"align":"full","backgroundColor":"grey-custom"} -->
+//                             <div class="wp-block-group alignfull has-grey-custom-background-color has-background"><!-- wp:media-text {"verticalAlignment":"top"} -->
+//                             <div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+//                             <h2></h2>
+//                             <!-- /wp:heading -->
+//                             <!-- wp:paragraph -->
+//                             <p></p>
+//                             <!-- /wp:paragraph -->
+//                             <!-- wp:buttons {"contentJustification":"right"} -->
+//                             <div class="wp-block-buttons is-content-justification-right"><!-- wp:button /-->
+//                             <!-- wp:button /--></div>
+//                             <!-- /wp:buttons --></div></div>
+//                             <!-- /wp:media-text --></div>
+//                             <!-- /wp:group -->
+//                             <!-- wp:group {"align":"full","backgroundColor":"white-custom"} -->
+//                             <div class="wp-block-group alignfull has-white-custom-background-color has-background"><!-- wp:media-text {"mediaPosition":"right","verticalAlignment":"top"} -->
+//                             <div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+//                             <h2></h2>
+//                             <!-- /wp:heading -->
+//                             <!-- wp:paragraph -->
+//                             <p></p>
+//                             <!-- /wp:paragraph -->
+//                             <!-- wp:buttons {"contentJustification":"right"} -->
+//                             <div class="wp-block-buttons is-content-justification-right"><!-- wp:button /-->
+//                             <!-- wp:button /--></div>
+//                             <!-- /wp:buttons --></div></div>
+//                             <!-- /wp:media-text --></div>
+//                             <!-- /wp:group -->
+//                             <!-- wp:group {"align":"full","backgroundColor":"grey-custom"} -->
+//                             <div class="wp-block-group alignfull has-grey-custom-background-color has-background"><!-- wp:media-text {"verticalAlignment":"top"} -->
+//                             <div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+//                             <h2></h2>
+//                             <!-- /wp:heading -->
+//                             <!-- wp:paragraph -->
+//                             <p></p>
+//                             <!-- /wp:paragraph -->
+//                             <!-- wp:buttons {"contentJustification":"right"} -->
+//                             <div class="wp-block-buttons is-content-justification-right"><!-- wp:button /-->
+//                             <!-- wp:button /--></div>
+//                             <!-- /wp:buttons --></div></div>
+//                             <!-- /wp:media-text --></div>
+//                             <!-- /wp:group -->
+//                             <!-- wp:group {"align":"full","backgroundColor":"white-custom"} -->
+//                             <div class="wp-block-group alignfull has-white-custom-background-color has-background"><!-- wp:media-text {"mediaPosition":"right","verticalAlignment":"top"} -->
+//                             <div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:heading -->
+//                             <h2></h2>
+//                             <!-- /wp:heading -->
+//                             <!-- wp:paragraph -->
+//                             <p></p>
+//                             <!-- /wp:paragraph -->
+//                             <!-- wp:buttons {"contentJustification":"right"} -->
+//                             <div class="wp-block-buttons is-content-justification-right"><!-- wp:button /-->
+//                             <!-- wp:button /--></div>
+//                             <!-- /wp:buttons --></div></div>
+//                             <!-- /wp:media-text --></div>
+//                             <!-- /wp:group -->',
+
+//     )
+// );
